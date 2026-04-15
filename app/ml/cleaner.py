6b40cb5efce0c73d71aabdaab9deb7a297e75fc2
Eby JK <ebyjkavungal@gmail.com>
2026-04-15 19:14:43 +0530

    nltk issue

--- app/ml/cleaner.py
+++ app/ml/cleaner.py
@@ -1,26 +1,52 @@
+# import re
+# import nltk
+# from nltk.corpus import stopwords
+
+
+# import nltk
+
+# try:
+#     nltk.data.find("corpora/stopwords")
+# except:
+#     nltk.download("stopwords")
+
+
+# # Make sure stopwords are downloaded at startup
+# nltk.download('stopwords', quiet=True)
+# STOP_WORDS = set(stopwords.words("english"))
+
+# def clean_email_text(text: str) -> str:
+#     text = str(text).lower()
+#     text = re.sub(r'\S+@\S+', ' ', text)       # remove emails
+#     text = re.sub(r'http\S+', ' ', text)       # remove URLs
+#     text = re.sub(r'\d+', ' ', text)           # remove numbers
+#     text = re.sub(r'[^a-z\s]', ' ', text)      # remove punctuation
+#     text = re.sub(r'\s+', ' ', text).strip()
+
+#     tokens = [w for w in text.split() if w not in STOP_WORDS]
+#     return " ".join(tokens)
+
+
 import re
 import nltk
 from nltk.corpus import stopwords
 
+# Lazy load stopwords (safe for deployment)
+def get_stopwords():
+    try:
+        return set(stopwords.words("english"))
+    except LookupError:
+        nltk.download("stopwords")
+        return set(stopwords.words("english"))
 
-import nltk
-
-try:
-    nltk.data.find("corpora/stopwords")
-except:
-    nltk.download("stopwords")
-
-
-# Make sure stopwords are downloaded at startup
-nltk.download('stopwords', quiet=True)
-STOP_WORDS = set(stopwords.words("english"))
+STOP_WORDS = get_stopwords()
 
 def clean_email_text(text: str) -> str:
     text = str(text).lower()
-    text = re.sub(r'\S+@\S+', ' ', text)       # remove emails
-    text = re.sub(r'http\S+', ' ', text)       # remove URLs
-    text = re.sub(r'\d+', ' ', text)           # remove numbers
-    text = re.sub(r'[^a-z\s]', ' ', text)      # remove punctuation
+    text = re.sub(r'\S+@\S+', ' ', text)
+    text = re.sub(r'http\S+', ' ', text)
+    text = re.sub(r'\d+', ' ', text)
+    text = re.sub(r'[^a-z\s]', ' ', text)
     text = re.sub(r'\s+', ' ', text).strip()
 
     tokens = [w for w in text.split() if w not in STOP_WORDS]
